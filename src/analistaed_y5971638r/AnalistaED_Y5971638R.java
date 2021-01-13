a219c05035d71dc53402f7361769ba7d0f71c111
Melissa Y5971638R <melissa_04@hotmail.com>
2021-01-13 20:28:38 +0100

    version final con javadoc DNI Y5971638R

--- src/analistaed_y5971638r/AnalistaED_Y5971638R.java
+++ src/analistaed_y5971638r/AnalistaED_Y5971638R.java
@@ -1,17 +1,35 @@
+
 package analistaed_y5971638r;
 
-
+/**
+ * Ejercicio practico EVAL 1 ED
+ * @author ANGIE MELISSA CANDELA DNI Y5971638R
+ * @version 1.0
+ */
 public class AnalistaED_Y5971638R {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    /**Contructor con 3 parametros
+     * 
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de indentidad con letras y sin espacios.
+     */
     public AnalistaED_Y5971638R(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
+    
+    /**Calcula el salario bruto mensual en función del salario base por hora y
+    las horas extras. El precio por hora de la hora extra se determina con la
+    constante FACTOR_HORA_EXTRA
+    * 
+    * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero).
+    * @return devuelve el salario bruto mensual en euros con dos decimales
 
+    */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
@@ -21,8 +39,7 @@ public class AnalistaED_Y5971638R {
     }
 
     // Este método no lo comentaremos con JavaDoc
-    public static void main(String[] args) {
-        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+    public static void main(String[] args) {      
         AnalistaED_Y5971638R emp = new AnalistaED_Y5971638R("Melissa Candela", "Y5971638R");
         int horasExtra = 2;
         System.out.println("El salario de " + emp.nombreCompleto 
@@ -30,3 +47,4 @@ public class AnalistaED_Y5971638R {
                 + " euros, al hacer " + horasExtra + " horas extra.");
     }
 }
+
